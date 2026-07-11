1e338678038d1e318d9298fe3ee29c11392ed537
Aayush Yadav <120627596+aayushyadav220803@users.noreply.github.com>
2026-07-12 00:15:27 +0530

    frontend changes1

--- backend/main.py
+++ backend/main.py
@@ -12,8 +12,8 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://ai-stock-assistant-gamma.vercel.app","http://localhost:5173"],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
